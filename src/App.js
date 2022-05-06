9dae82469a219cc04d051dfd2d646d980d02c4c1
tongfeiguo <tongfei.gf@gmail.com>
2022-05-06 13:11:29 -0400

    react login

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import './vendors/bootstrap/css/bootstrap.min.css';
-import './vendors/bootstrap/bootstrap.min.css';
-import './vendors/fontawesome/css/all.min.css';
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import './App.css';
 import Login from "./Cativate/Log-in/index"
 
 function App() {
     return (
-        <Login/>
-        // <BrowserRouter>
-        //   <div className="container">
-        //     <Routes>
-        //       <Route path="/">
-        //         <Route index element={<Login/>}/>
-        //         <Route path="login" exact={true} element={<Login/>}/>
-        //     </Routes>
-        //   </div>
-        // </BrowserRouter>
-    );
+        <BrowserRouter>
+            <div className="container">
+                <Routes>
+                    <Route path="/">
+                        <Route index element={<Login/>}/>
+                        <Route path="login" exact={true} element={<Login/>}/>
+                    </Route>
+                </Routes>
+            </div>
+        </BrowserRouter>
+);
 }
 
 export default App;
--- src/Cativate/Log-in/index.js
+++ src/Cativate/Log-in/index.js
@@ -1,24 +1,52 @@
 import React from "react";
+import '../Style/login.css';
+
 import {Link} from "react-router-dom";
-import Classes from "./Classes";
-import Styles from "./Styles";
-import ConditionalOutput from "./ConditionalOutput";
-import TodoItem from "./Todo/TodoItem";
-import TodoList from "./Todo/TodoList"
-import ReduxExamples from "./ReduxExamples/components";
-const Labs = () => {
-    return(
+
+const Login = () => {
+    return (
         <div>
-            <h1>Tuiter</h1>
-            <h2>Home Screen</h2>
-            <Link to="/tuiter">
-                Tuiter Home
-            </Link>
-            <h2>Explore Screen</h2>
-            <Link to="/tuiter/explore">
-                Tuiter Explore Screen
-            </Link>
+            {/* <!--Left part: Introduction--> */}
+            <div class="intro">
+                <h1 class="ft-48px">Hi, I am "CAT"</h1>
+                <h2 class="ft-28px">I will be your Facebook ADs Manager!</h2>
+            </div>
+            
+            {/* <!--Right part: Sign-up Panel--> */}
+            <div class="container">
+                <div class="card_bg rounded-50px border-thin border-green border-solid pd-50px">
+                    <p class="Header1"> Sign in to Cactivate</p>
+                    {/* <!--Username--> */}
+                    <div>
+                        <p class="Header2">Username or Email Address</p>
+                        <p class="rounded-10px border-thin border-green border-solid pd-30px opacity"></p>
+                    </div>
+                    {/* <!--Password--> */}
+                    <div>
+                        <p class="Header2">Password</p>
+                        <p class="rounded-10px border-thin border-green border-solid pd-30px opacity">
+                            8+ Characters
+                        </p>
+                    </div>
+                    {/* <!--Button--> */}
+                    <button
+                        class="button rounded-10px border-thin border-green border-solid pd-10px bg-green mg-20px button_text">
+                        Sign in
+                    </button>
+                </div>
+            </div>
         </div>
+        // <div>
+        //     <h1>Tuiter</h1>
+        //     <h2>Home Screen</h2>
+        //     <Link to="/tuiter">
+        //         Tuiter Home
+        //     </Link>
+        //     <h2>Explore Screen</h2>
+        //     <Link to="/tuiter/explore">
+        //         Tuiter Explore Screen
+        //     </Link>
+        // </div>
     )
 };
-export default Labs;
+export default Login;
